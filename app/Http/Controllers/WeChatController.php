9e3d4d2a6954ec1497f4a41693f0e7f90904e1d8
benlau921 <g1140563@study.ouhk.edu.hk>
2018-11-27 16:16:49 +0800

    fjekjf

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -18,7 +18,7 @@ class WeChatController extends Controller
 
         $app = app('wechat.official_account');
         $app->server->push(function($message){
-            return "欢迎关注 overtrue！";
+            return "Hello Ben";
         });
 
         return $app->server->serve();
