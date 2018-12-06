47ab22f77fc5719ff7a0b636e99ecf239f02c138
benlau921 <g1140563@study.ouhk.edu.hk>
2018-12-06 14:59:31 +0800

    file

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -112,7 +112,7 @@ class WeChatController extends Controller {
 					case "orderlike":
 					case "jrn-process-a":
 					case "jrn-process-b":
-						return "<a href = \"http://www.orderlikepnv.com/wp-external/wechat-show-jrn.php?ref=300015&jrntax=process-selfiemov-borrow\">Journal B</a>"."diu nei";
+						return "<a href = \"http://www.orderlikepnv.com/wp-external/wechat-show-jrn.php?ref=300015&jrntax=process-selfiemov-borrow\">Journal B</a>"."Journal B";
 						break;
 					case "jrn-process-c":
 					case "jrn-process-d":
